47b140fbfc636f6eb9c43d4f80ccfaa8ced4c142
fcarri <fcs0000@gmail.com>
2022-10-06 19:08:31 +0200

    Commit 9

---
+++ ta05_1.java
@@ -0,0 +1,17 @@
+package project5;
+
+public class ta05_1 {
+public static void main(String args[]){
+		
+		int a = 1;
+		int b = 2;
+		
+		if (a == b){
+			System.out.println("a = b");
+		}else if(a > b){
+			System.out.println("a > b");
+		}else{
+			System.out.println("a < b");	
+		}
+	}
+}
